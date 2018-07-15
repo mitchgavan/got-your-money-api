303806eae0800447875ae17d45a00c0cc6668ece
Mitch Gavan <mitchgavan@gmail.com>
2018-07-15 10:42:06 +1000

    refactor: create item handler

--- handler.js
+++ handler.js
@@ -1,50 +1,20 @@
 require('dotenv').config()
 const connectToDatabase = require('./db')
-const ItemModel = require('./model/Item')
-
-const createErrorResponse = (statusCode, message) => ({
-  statusCode: statusCode || 501,
-  headers: { 'Content-Type': 'text/plain' },
-  body: message || 'Incorrect Id',
-})
+const Item = require('./model/Item')
 
 module.exports.createItem = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false
 
-  const data = JSON.parse(event.body)
-  
-  const item = new ItemModel({
-    id: data.id,
-    title: data.title,
-    cost: data.cost,
-    date: data.date
-  })
-
-  const errors = item.validateSync()
-
-  if (errors) {
-    console.log(errors)
-    db.close()
-    callback(null, createErrorResponse(400, 'Invalid data supplied'))
-  }
-
   connectToDatabase().then(() => {
-    item
-      .save()
-      .then(() => {
-        callback(null, {
-          statusCode: 200,
-          body: JSON.stringify({ 
-            message: 'Item created successfully',
-            id: item.id
-          }),
-        })
-      })
-      .catch((err) => {
-        callback(null, createErrorResponse(err.statusCode, err.message))
-      })
-      .finally(() => {
-        db.close()
-      })
+    Item.create(JSON.parse(event.body))
+      .then(item => callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(item)
+      }))
+      .catch(err => callback(null, {
+        statusCode: err.statusCode || 500,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Could not create the item.'
+      }))
   })
 }
