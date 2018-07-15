0d6cf7500da2cec663770ff951b5501d7ba5b16b
Mitch Gavan <mitchgavan@gmail.com>
2018-07-15 18:12:19 +1000

    feat: update handler/route

--- handler.js
+++ handler.js
@@ -53,6 +53,28 @@ module.exports.getAll = (event, context, callback) => {
           headers: { 'Content-Type': 'text/plain' },
           body: 'Could not fetch the items.'
         }))
+    })
+}
+
+module.exports.update = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false
+
+  connectToDatabase()
+    .then(() => {
+      Item.findByIdAndUpdate(
+          event.pathParameters.id,
+          JSON.parse(event.body),
+          { new: true }
+      )
+        .then(item => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(item)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the items.'
+        }));
     });
 };
 
